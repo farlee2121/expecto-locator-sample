cd25f64075899071d1dc392f8d5b491fb3872f0b
farlee2121 <2847259+farlee2121@users.noreply.github.com>
2026-05-15 21:12:18 -0500

    Update to the project-based AST discovery

--- Expecto-Locator-Sample/Main.fs
+++ Expecto-Locator-Sample/Main.fs
@@ -1,7 +1,6 @@
 ﻿module Expecto_Locator_Sample
 open Expecto
 
-TopLevelDefaults.testLocator <- Expecto.TestLocator.CompilerService.FCSTestLocator.testLocator
 [<EntryPoint>]
 let main argv =
     Tests.runTestsInAssemblyWithCLIArgs [] argv
